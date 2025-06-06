f5cc369bbeb7baa3d8374ebd011e42241b4ea100
benhanrahan12123 <benhanrahan23424@protonmail.com>
2025-06-06 17:21:55 +0100

    chmod +x

--- copyScripts/laptop_dotfiles.sh
+++ copyScripts/laptop_dotfiles.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Moving Hyprland configs..."
-rsync -avh --progress ~/dotfiles/Laptop/hypr/.config/hypr/ ~/.config/hypr/
+rsync -avh --progress ~/dotfiles/laptop/hypr/.config/hypr/ ~/.config/hypr/
 
 echo "Moving Waybar configs..."
-rsync -avh --progress ~/dotfiles/Laptop/waybar/.config/waybar/ ~/.config/waybar/
+rsync -avh --progress ~/dotfiles/laptop/waybar/.config/waybar/ ~/.config/waybar/
 
 echo "Done ✅"
