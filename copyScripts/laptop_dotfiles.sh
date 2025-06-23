8c986f6f10a37c37d2d7a1bbd7b3e8cbe43bee47
benhanrahan12123 <benhanrahan23424@protonmail.com>
2025-06-23 17:48:14 +0100

    updated file path in scripts

--- copyScripts/laptop_dotfiles.sh
+++ copyScripts/laptop_dotfiles.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Moving Hyprland configs..."
-rsync -avh --progress ~/dotfiles/laptop/hypr/.config/hypr/ ~/.config/hypr/
+rsync -avh --progress ~/Documents/dotfiles/laptop/hypr/.config/hypr/ ~/.config/hypr/
 
 echo "Moving Waybar configs..."
-rsync -avh --progress ~/dotfiles/laptop/waybar/.config/waybar/ ~/.config/waybar/
+rsync -avh --progress ~/Documents/dotfiles/laptop/waybar/.config/waybar/ ~/.config/waybar/
 
 echo "Done ✅"
--- copyScripts/pc_dotfiles.sh
+++ copyScripts/pc_dotfiles.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Moving Hyprland configs..."
-rsync -avh --progress ~/dotfiles/pc/hypr/.config/hypr/ ~/.config/hypr/
+rsync -avh --progress ~/Documents/dotfiles/pc/hypr/.config/hypr/ ~/.config/hypr/
 
 echo "Moving Waybar configs..."
-rsync -avh --progress ~/dotfiles/pc/waybar/.config/waybar/ ~/.config/waybar/
+rsync -avh --progress ~/Documents/dotfiles/pc/waybar/.config/waybar/ ~/.config/waybar/
 
 echo "Done ✅"
