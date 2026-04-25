f9b567bb1657818cf856283da3dfc390285ff3b8
benhanrahan12123 <benhanrahan23424@protonmail.com>
2026-04-25 17:52:24 +0100

    keymaps & autocmds

---
+++ current/nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,34 @@
+
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+-- Buffer navigation
+vim.keymap.set("n", "<leader>bn", "<Cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", "<Cmd>bprevious<CR>", { desc = "Previous buffer" })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right window" })
+
+-- Splitting & Resizing
+vim.keymap.set("n", "<leader>sv", "<Cmd>vsplit<CR>" , { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<Cmd>split<CR>" , { desc = "Split window horizontally" })
+vim.keymap.set("n", "<C-Up>", "<Cmd>resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<Cmd>resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<Cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Better J behaviour
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+-- Quick config editing
+vim.keymap.set("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Edit Config" })
